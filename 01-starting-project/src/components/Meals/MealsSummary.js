97850b85c4c3412f1e594bc7df08c286f2c734f2
lucic15 <lucicluka31@gmail.com>
2023-04-19 20:19:56 +0200

    4

---
+++ 01-starting-project/src/components/Meals/MealsSummary.js
@@ -0,0 +1,17 @@
+
+import classes from './MealsSummary.module.css';
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Borise</h2>
+      <p>
+        Popusi mi ga
+      </p>
+      <p>
+      </p>
+    </section>
+  );
+};
+
+export default MealsSummary;
